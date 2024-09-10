1c67e2b22853bf1a184c1469a9cb056902fe6b4e
mnipon <78396926+mnipon@users.noreply.github.com>
2024-09-10 09:56:16 +0700

    mod: try again2

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Nipon Lambda2 by Github action!')
+        'body': json.dumps('Hello from Nipon Lambda3 by Github action!')
     }
